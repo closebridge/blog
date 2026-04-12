6a042705fc3bf6f3b85657f74745aee28137cebe
closebridge <108937010+closebridge@users.noreply.github.com>
2026-04-13 01:43:49 +0700

    fix(vite.cf, footer.svelte): covered base, better format

--- src/template/footer.svelte
+++ src/template/footer.svelte
@@ -1,28 +1,33 @@
 <div
-	class="border-t-2 border-(--secondary-text) flex flex-row justify-between items-end py-6 px-2"
+	class="border-t-2 border-(--secondary-text) flex flex-col md:flex-row justify-between items-start md:items-end py-6 px-2"
 >
-	<div class="*:block secondary-text">
-		<p class="font-thin">nogisoft.work</p>
-		<a href="/" class="font-medium">home page</a>
-		<a href="/" class="font-medium">my portfolio</a>
-		<a href="/" class="font-medium">my blog</a>
+	<div class="grid grid-cols-2 justify-evenly gap-4">
+		<div class="*:block secondary-text">
+			<p class="font-thin">nogisoft.work</p>
+			<a href="/" class="font-medium">home page</a>
+			<a href="/" class="font-medium">my portfolio</a>
+			<a href="/" class="font-medium">my blog</a>
+		</div>
+
+		<div class="*:block secondary-text">
+			<p class="font-thin">nsense.nogisoft.work</p>
+			<a href="/" class="font-medium">about nSense</a>
+			<a href="/" class="font-medium">migration assistant</a>
+			<a href="/" class="font-medium">status page</a>
+		</div>
+
+		<div class="*:block secondary-text">
+			<p class="font-thin">hardware.nogisoft.work</p>
+			<a href="/" class="font-medium">lol?</a>
+			<a href="/" class="font-medium">lmfao?</a>
+			<a href="/" class="font-medium">bru?</a>
+		</div>
 	</div>
 
-	<div class="*:block secondary-text">
-		<p class="font-thin">nsense.nogisoft.work</p>
-		<a href="/" class="font-medium">about nSense</a>
-		<a href="/" class="font-medium">migration assistant</a>
-		<a href="/" class="font-medium">status page</a>
-	</div>
+	<!-- whatever, looks good for smol window -->
+	<div class="md:hidden"><br /><br /></div>
 
-	<div class="*:block secondary-text">
-		<p class="font-thin">hardware.nogisoft.work</p>
-		<a href="/" class="font-medium">lol?</a>
-		<a href="/" class="font-medium">lmfao?</a>
-		<a href="/" class="font-medium">bru?</a>
-	</div>
-
-	<div class="*:block secondary-text text-end">
+	<div class="*:block secondary-text text-start md:text-end">
 		<p>nogi, {new Date().getFullYear()}</p>
 		<p>no tracking. some hook states. also api calls.</p>
 	</div>
