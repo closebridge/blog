b8ef3b85aa95d885086867a6c9003d624cc804ba
closebridge <108937010+closebridge@users.noreply.github.com>
2026-04-24 20:07:36 +0700

    feat(title.svelte): add extra feature button with callback, fu props

--- src/template/components/title.svelte
+++ src/template/components/title.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	// and i still have no fucking clue on how 2 pass props
 	// what happened to the old way?
-	export const {
-		numberCount,
-		title,
-		classes,
-	}: { numberCount: number; title: string; classes?: string } = $props();
+	interface TitleProps {
+		numberCount: number;
+		title: string;
+		classes?: string;
+		extraFeature?: false | { icon: string; func: () => boolean };
+	}
+
+	export const { numberCount, title, classes, extraFeature }: TitleProps =
+		$props();
 </script>
 
 <div>
@@ -14,5 +18,15 @@
 			0{numberCount}.
 		</p>
 		<p id="title-header">{title}</p>
+		{#if extraFeature}
+			<button
+				onclick={async () => {
+					await extraFeature.func();
+				}}
+				class="material-symbols-rounded cursor-pointer p-4 md:p-2"
+				style="font-size: 18px; color: var(--secondary-text)"
+				>{extraFeature.icon}</button
+			>
+		{/if}
 	</div>
 </div>
