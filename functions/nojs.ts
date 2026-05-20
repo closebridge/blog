dd5cfd16a11f3ee86cab5044769197e6e3e529a4
closebridge <108937010+closebridge@users.noreply.github.com>
2026-05-21 00:09:35 +0700

    chore(nots-baseheadhtml): wont ship trash across clients

--- functions/nojs.ts
+++ functions/nojs.ts
@@ -117,23 +117,34 @@ async function renderHTML(
 				<meta charset="UTF-8" />
 				<link rel="icon" type="image/svg+xml" href="/favicon.ico" />
 
-				<link rel="stylesheet" href="/assets/index--nojs.css" />
+				${
+					!isLegacyBrowser
+						? `<link rel="stylesheet" href="/assets/index--nojs.css" />`
+						: ""
+				}
 
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0"
-				/>
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link
-					href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0"
-				/>
+
+				${
+					!isLegacyBrowser
+						? `<link rel="preconnect" href="https://fonts.googleapis.com" />
+						<link
+							rel="stylesheet"
+							href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0"
+						/>
+						<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+						<link
+							href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
+							rel="stylesheet"
+						/>
+						<link
+							rel="stylesheet"
+							href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0"
+						/>`
+						: ""
+				}
+
+
 
 				<!-- Embeds -->
 				<meta property="og:title" content="nogc's blog" />
